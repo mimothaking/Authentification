66156060c701ecc4d0a27226e04d434a39772154
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-07-31 18:15:35 +0100

    Update homeScreen.js

--- AuthFirebase/screens/homeScreen.js
+++ AuthFirebase/screens/homeScreen.js
@@ -1,12 +1,25 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { auth } from '../firebase/firebase'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+
+    const navigation = useNavigation()
+
+    const handleLogOut = () => {
+        auth.signOut().then(() => {
+            navigation.replace("Login")
+        })
+        .catch(error => alert(error.message))
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Email :</Text>
+      <Text>Email :{auth.currentUser?.email}</Text>
       <TouchableOpacity
       style={styles.button}
+      onPress={handleLogOut}
       >
         <Text style={styles.buttonText}>SignOut</Text>
       </TouchableOpacity>
@@ -24,7 +37,7 @@ const styles = StyleSheet.create({
     },
     button: {
         backgroundColor: '#b4c4d4',
-        width: "100%",
+        width: "60%",
         padding: 15,
         borderRadius: 10,
         alignItems: 'center',
