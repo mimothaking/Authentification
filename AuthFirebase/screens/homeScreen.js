073583f88d9de4b9ed7998b06a91acff840e917d
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-08-05 23:59:35 +0100

    Some updates

--- AuthFirebase/screens/homeScreen.js
+++ AuthFirebase/screens/homeScreen.js
@@ -17,9 +17,24 @@ const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.username}>Welcome, {auth.currentUser?.email.substring(0, auth.currentUser?.email.lastIndexOf("@"))}</Text>
+
       <TouchableOpacity
-      style={styles.button}
-      onPress={handleLogOut}
+        style={styles.buttonChoice}
+        onPress={() => {}}
+      >
+        <Text style={styles.buttonChoiceText}>Weather</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonChoice2}
+        onPress={() => { }}
+      >
+        <Text style={styles.buttonChoiceText2}>Quiz Game</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleLogOut}
       >
         <Text style={styles.buttonText}>SignOut</Text>
       </TouchableOpacity>
@@ -39,7 +54,7 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 10,
         alignItems: 'center',
-        marginTop: 590,
+        marginTop: 190,
         marginLeft: 75,
     },
     buttonText: {
@@ -51,5 +66,33 @@ const styles = StyleSheet.create({
       fontSize: 14,
       marginTop: '10%',
       marginLeft: '65%'
-    }
+    },
+  buttonChoice: {
+    backgroundColor: '#b4c4d4',
+    width: "50%",
+    padding: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 90,
+    marginLeft: 88,
+  },
+  buttonChoiceText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16
+  },
+  buttonChoice2: {
+    backgroundColor: '#b4c4d4',
+    width: "50%",
+    padding: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 90,
+    marginLeft: 88,
+  },
+  buttonChoiceText2: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16
+  },
 })
