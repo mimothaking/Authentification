4fc03eebdabe0bbfc68f0dd678b0880f41e2f9ae
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-08-16 18:19:23 +0100

    updates

--- AuthFire-Quiz-Weather/App.js
+++ AuthFire-Quiz-Weather/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
